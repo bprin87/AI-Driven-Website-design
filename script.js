2e93e9a077af09a2a435fa1832f3967bd3bc006b
Ben <123553196+bprin87@users.noreply.github.com>
2024-02-13 13:34:30 +0000

    Added swap function
    Allows users to swap the departure and destination inputs

--- script.js
+++ script.js
@@ -6,3 +6,28 @@ mobileMenuBtn.addEventListener('click', function() {
     mobileMenuBtn.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
 });
+
+// function to swap departure and destination inputs
+function swap() {
+
+    let fromInput = document.querySelector('#fromInput');
+    let toInput = document.querySelector('#toInput');
+
+    let tempValue = fromInput.value;
+    fromInput.value = toInput.value;
+    toInput.value = tempValue;
+
+    let tempPlaceholder = fromInput.placeholder;
+    fromInput.placeholder = toInput.placeholder;
+    toInput.placeholder = tempPlaceholder;
+
+    // if label is from, from input must say where from
+    // if label is to, to input must say where to
+    let departureLabel = document.querySelector('#from');
+    
+    if (departureLabel.innerText === 'From') {
+        fromInput.placeholder = 'Where from?';
+        toInput.placeholder = 'Where to?'
+    } 
+
+}
