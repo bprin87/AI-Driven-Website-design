643914a784a7e5c19b6d49c53ede7c75f7235bf6
Ben <123553196+bprin87@users.noreply.github.com>
2024-02-14 20:03:11 +0000

    Added function for zooming on map route
    Added a zoom in and zoom out button to view flight destinations

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ mobileMenuBtn.addEventListener('click', function() {
     mobileMenu.classList.toggle('is-active');
 });
 
-// function to swap departure and destination inputs
+// Swap departure and destination inputs
 function swap() {
 
     let fromInput = document.querySelector('#fromInput');
@@ -29,5 +29,58 @@ function swap() {
         fromInput.placeholder = 'Where from?';
         toInput.placeholder = 'Where to?'
     } 
-
 }
+
+// Hide return date if one way trip is selected
+let returnDate = document.querySelector('.return-date');
+let selectTripType = document.querySelector('#trip');
+
+selectTripType.addEventListener('change', (event) => {
+    
+    let selectedValue = event.target.value;
+
+    if (selectedValue === 'one-way') {
+        returnDate.classList.add('return-date-none');
+    } else {
+        returnDate.classList.remove('return-date-none');
+    }
+    
+});
+
+
+// Zoom in and out of world map
+
+// get original width of map
+let originalWidth;
+
+window.onload = function() {
+    
+    let map = document.querySelector('.map-container img');
+    originalWidth = map.clientWidth;
+}
+
+// Zoom in by 100px
+function zoomIn() {
+
+    let map = document.querySelector('.map-container img');
+    let currentWidth = map.clientWidth;
+    
+    map.style.width = (currentWidth + 100) + 'px';
+    
+}
+
+// Zoom out by 100px
+function zoomOut() {
+
+    let map = document.querySelector('.map-container img');
+    let currentWidth = map.clientWidth;
+    
+    // if the current width is greater than the original width
+    // zoom out
+    if (currentWidth > originalWidth){
+        map.style.width = (currentWidth - 100) + 'px'
+    }
+    
+}
+
+
